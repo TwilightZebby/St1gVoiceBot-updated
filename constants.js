3dbf305bb2359305d0d572812f3138f10de365cf
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-09-19 12:53:26 +0100

    Initial commit

---
+++ constants.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+module.exports =
+{
+    // Discord Client representing the Bot/App
+    DiscordClient: new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildIntegrations ] }),
+
+    // Collections that are used in many locations
+    Collections: {
+        TextCommands: new Collection(),
+        SlashCommands: new Collection(),
+        ContextCommands: new Collection(),
+        Buttons: new Collection(),
+        Selects: new Collection(),
+        Modals: new Collection(),
+
+        TextCooldowns: new Collection(),
+        SlashCooldowns: new Collection(),
+        ContextCooldowns: new Collection(),
+        ButtonCooldowns: new Collection(),
+        SelectCooldowns: new Collection()
+    }
+}
---
+++ index.js
@@ -0,0 +1,206 @@
+const { RateLimitError, DMChannel } = require("discord.js");
+const fs = require("fs");
+
+const { DiscordClient, Collections } = require("./constants.js");
+const Config = require("./config.js");
+
+
+
+/******************************************************************************* */
+// BRING IN FILES FOR COMMANDS AND INTERACTIONS
+// Text Commands
+const TextCommandFiles = fs.readdirSync("./TextCommands").filter(file => file.endsWith(".js"));
+for ( const File of TextCommandFiles )
+{
+    const TempCommand = require(`./TextCommands/${File}`);
+    Collections.TextCommands.set(TempCommand.Name, TempCommand);
+}
+
+// Slash Commands
+const SlashCommandFiles = fs.readdirSync("./Interactions/SlashCommands").filter(file => file.endsWith(".js"));
+for ( const File of SlashCommandFiles )
+{
+    const TempCommand = require(`./Interactions/SlashCommands/${File}`);
+    Collections.SlashCommands.set(TempCommand.Name, TempCommand);
+}
+
+// Context Commands
+const ContextCommandFiles = fs.readdirSync("./Interactions/ContextCommands").filter(file => file.endsWith(".js"));
+for ( const File of ContextCommandFiles )
+{
+    const TempCommand = require(`./Interactions/ContextCommands/${File}`);
+    Collections.ContextCommands.set(TempCommand.Name, TempCommand);
+}
+
+// Buttons
+const ButtonFiles = fs.readdirSync("./Interactions/Buttons").filter(file => file.endsWith(".js"));
+for ( const File of ButtonFiles )
+{
+    const TempButton = require(`./Interactions/Buttons/${File}`);
+    Collections.Buttons.set(TempButton.Name, TempButton);
+}
+
+// Selects
+const SelectFiles = fs.readdirSync("./Interactions/Selects").filter(file => file.endsWith(".js"));
+for ( const File of SelectFiles )
+{
+    const TempSelect = require(`./Interactions/Selects/${File}`);
+    Collections.Selects.set(TempSelect.Name, TempSelect);
+}
+
+// Modals
+const ModalFiles = fs.readdirSync("./Interactions/Modals").filter(file => file.endsWith(".js"));
+for ( const File of ModalFiles )
+{
+    const TempModal = require(`./Interactions/Modals/${File}`);
+    Collections.Modals.set(TempModal.Name, TempModal);
+}
+
+
+
+
+
+
+
+
+/******************************************************************************* */
+// DISCORD - READY EVENT
+DiscordClient.once('ready', () => {
+    DiscordClient.user.setPresence({ status: 'online' });
+    console.log(`${DiscordClient.user.username}#${DiscordClient.user.discriminator} is online and ready!`);
+});
+
+
+
+
+
+
+
+
+/******************************************************************************* */
+// DEBUGGING AND ERROR LOGGING
+// Warnings
+process.on('warning', (warning) => { return console.warn("***WARNING: ", warning); });
+DiscordClient.on('warn', (warning) => { return console.warn("***DISCORD WARNING: ", warning); });
+
+// Unhandled Promise Rejections
+process.on('unhandledRejection', (err) => { return console.error("***UNHANDLED PROMISE REJECTION: ", err); });
+
+// Discord Errors
+DiscordClient.on('error', (err) => { return console.error("***DISCORD ERROR: ", err); });
+
+// Discord Rate Limit - Only uncomment when debugging
+//DiscordClient.rest.on('rateLimited', (RateLimitError) => { return console.log("***DISCORD RATELIMIT HIT: ", RateLimitError); });
+
+
+
+
+
+
+
+
+/******************************************************************************* */
+// DISCORD - MESSAGE CREATE EVENT
+const TextCommandHandler = require("./BotModules/Handlers/TextCommandHandler.js");
+
+DiscordClient.on('messageCreate', async (message) => {
+    // Partials
+    if ( message.partial ) { return; }
+
+    // Bots
+    if ( message.author.bot ) { return; }
+
+    // System Messages
+    if ( message.system || message.author.system ) { return; }
+
+    // DM Channel Messages
+    if ( message.channel instanceof DMChannel ) { return; }
+
+    // Safe-guard against Discord Outages
+    if ( !message.guild.available ) { return; }
+
+
+
+    // Check for (and handle) Commands
+    let textCommandStatus = await TextCommandHandler.Main(message);
+    if ( textCommandStatus === false )
+    {
+        // No Command detected
+        return;
+    }
+    else if ( textCommandStatus === null )
+    {
+        // Prefix was detected, but wasn't a command on the bot
+        return;
+    }
+    else
+    {
+        // Command failed or successful
+        return;
+    }
+});
+
+
+
+
+
+
+
+
+/******************************************************************************* */
+// DISCORD - INTERACTION CREATE EVENT
+const SlashCommandHandler = require("./BotModules/Handlers/SlashCommandHandler.js");
+const ContextCommandHandler = require("./BotModules/Handlers/ContextCommandHandler.js");
+const ButtonHandler = require("./BotModules/Handlers/ButtonHandler.js");
+const SelectHandler = require("./BotModules/Handlers/SelectHandler.js");
+const AutocompleteHandler = require("./BotModules/Handlers/AutocompleteHandler.js");
+const ModalHandler = require("./BotModules/Handlers/ModalHandler.js");
+
+DiscordClient.on('interactionCreate', async (interaction) => {
+    if ( interaction.isChatInputCommand() )
+    {
+        // Slash Command
+        return await SlashCommandHandler.Main(interaction);
+    }
+    else if ( interaction.isContextMenuCommand() )
+    {
+        // Context Command
+        return await ContextCommandHandler.Main(interaction);
+    }
+    else if ( interaction.isButton() )
+    {
+        // Button
+        return await ButtonHandler.Main(interaction);
+    }
+    else if ( interaction.isSelectMenu() )
+    {
+        // Select
+        return await SelectHandler.Main(interaction);
+    }
+    else if ( interaction.isAutocomplete() )
+    {
+        // Autocomplete
+        return await AutocompleteHandler.Main(interaction);
+    }
+    else if ( interaction.isModalSubmit() )
+    {
+        // Modal
+        return await ModalHandler.Main(interaction);
+    }
+    else
+    {
+        // Unknown or unhandled new type of Interaction
+        return console.log(`****Unrecognised or new unhandled Interaction type triggered:\n${interaction.type}\n${interaction}`);
+    }
+});
+
+
+
+
+
+
+
+
+/******************************************************************************* */
+
+DiscordClient.login(Config.TOKEN);
