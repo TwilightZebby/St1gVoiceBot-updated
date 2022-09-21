bb49f3d086e3cd6da3c387b5e0856cc7e9e7cc43
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-09-21 09:25:49 +0100

    Add JSON stuff to `/voice create`

--- Interactions/SlashCommands/voice.js
+++ Interactions/SlashCommands/voice.js
@@ -1,4 +1,5 @@
 const { ChatInputCommandInteraction, ChatInputApplicationCommandData, AutocompleteInteraction, ApplicationCommandType, ApplicationCommandOptionType, CategoryChannel, PermissionFlagsBits, PermissionsBitField, ChannelType } = require("discord.js");
+const fs = require('fs');
 const { DiscordClient, Collections } = require("../../constants.js");
 const LocalizedErrors = require("../../JsonFiles/errorMessages.json");
 const LocalizedStrings = require("../../JsonFiles/stringMessages.json");
@@ -107,10 +108,15 @@ async function createTempVoice(slashCommand)
     const VoiceCreator = slashCommand.member;
     const GuildId = slashCommand.guildId;
     const VoiceSettings = require('../../JsonFiles/hidden/guildSettings.json');
+    const ActiveTempVoices = require('../../JsonFiles/hidden/activeTempVoices.json');
+    const SearchableActiveTempVoices = Object.values(ActiveTempVoices);
     const GuildSettings = VoiceSettings[`${GuildId}`];
 
     // Verify Settings have been set
     if ( !GuildSettings ) { return await slashCommand.reply({ ephemeral: true, content: `This Server hasn't setup the Temp VC System yet!` }); }
+    // Verify User doesn't already have one active Temp VC created
+    const CheckExistingVC = SearchableActiveTempVoices.filter(item => item['CHANNEL_OWNER_ID'] === VoiceCreator.id);
+    if ( CheckExistingVC.length > 0 ) { return await slashCommand.reply({ ephemeral: true, content: `You cannot create a new Temp VC when you already have one created & active! ( <#${CheckExistingVC[0]['VOICE_CHANNEL_ID']}> )` }); }
 
     const ParentCategoryId = GuildSettings["PARENT_CATEGORY_ID"];
     /** @type {CategoryChannel} */
@@ -119,14 +125,13 @@ async function createTempVoice(slashCommand)
     // Verify Bot has Permissions
     /** @type {PermissionsBitField} */
     const BotPermissions = FetchedCategory.permissionsFor(DiscordClient.user.id);
-    if ( !BotPermissions.has(PermissionFlagsBits.ViewChannel) || !BotPermissions.has(PermissionFlagsBits.ManageChannels) || !BotPermissions.has(PermissionFlagsBits.ManageRoles) )
+    if ( !BotPermissions.has(PermissionFlagsBits.ViewChannel) || !BotPermissions.has(PermissionFlagsBits.ManageChannels) )
     {
         return await slashCommand.reply({ ephemeral: true, content: `Sorry, but I cannot create a new Temp Voice Channel for you, since I am missing required Permissions for the **<#${ParentCategoryId}>** Category. Please ask this Server's Admins or Owner to fix this!
 
 Permissions I require in **<#${ParentCategoryId}>** :
 - \`View Channels\`
-- \`Manage Channels\`
-- \`Manage Permissions\`` });
+- \`Manage Channels\`` });
     }
 
 
@@ -142,8 +147,19 @@ Permissions I require in **<#${ParentCategoryId}>** :
     })
     .then(async (CreatedVoiceChannel) => {
         // Voice Channel created
-        // ACK Slash Command first
+        // Save to JSON
+        let newVoice = {
+            "VOICE_CHANNEL_ID": CreatedVoiceChannel.id,
+            "CHANNEL_OWNER_ID": VoiceCreator.id
+        };
+        ActiveTempVoices[CreatedVoiceChannel.id] = newVoice;
+        fs.writeFile('./JsonFiles/hidden/activeTempVoices.json', JSON.stringify(ActiveTempVoices, null, 4), async (err) => {
+            if ( err ) { return await slashCommand.editReply({ ephemeral: true, content: `Sorry, something went wrong while trying to create your Temp Voice Channel... Please try again later` }); }
+        });
+
+        // ACK Slash Command
         await slashCommand.editReply({ content: `Created your Temp Voice Channel! ( <#${CreatedVoiceChannel.id}> )\nYou can find it under this Server's Temp VC Category (I've also pinged you in it!)` });
+        
         // Send a message in the VC (Thanks to TiV) containing a ping to help find it
         await CreatedVoiceChannel.send({ allowedMentions: { users: [VoiceCreator.id] }, content: `*Temp VC | Creator: <@${VoiceCreator.id}>*
 
